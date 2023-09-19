8485ca958732a754acf8960d465666dfd52bfcfc
Agustina Chiodini <agus.chio25790@gmail.com>
2023-09-18 21:22:38 -0300

    try another date

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
-$(function() {
-    // Configura el datepicker
-    $("#fechaSelect").datepicker({
-      dateFormat: "dd/mm/yy", // Formato de fecha
-    onSelect: function(dateText, inst) {
-        // Cuando se selecciona una fecha, actualiza el valor del select
-        $("#fechaSelect").val(dateText);
-    },
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona el elemento del input date y el span del marcador de posición
+  const dateInput = document.getElementById("date-input");
+  const placeholder = document.querySelector(".placeholder-date");
+
+  // Agrega un evento al input date para mostrar u ocultar el marcador de posición
+  dateInput.addEventListener("focus", function () {
+      placeholder.style.display = "none";
+  });
+
+  dateInput.addEventListener("blur", function () {
+      if (dateInput.value === "") {
+          placeholder.style.display = "block";
+      } else {
+          placeholder.style.display = "none";
+      }
+  });
 });
+
+
+// $(function() {
+//     // Configura el datepicker
+//     $("#fechaSelect").datepicker({
+//       dateFormat: "dd/mm/yy", // Formato de fecha
+//     onSelect: function(dateText, inst) {
+//         // Cuando se selecciona una fecha, actualiza el valor del select
+//         $("#fechaSelect").val(dateText);
+//     },
+//     });
+// });
 
