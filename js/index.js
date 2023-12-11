cda20348fabfca25f3299e17d9be7030ad97b2c8
Laura Escalante <laura088.le@gmail.com>
2023-12-11 11:08:04 -0300

    fetch plantas y medicamentos

--- js/index.js
+++ js/index.js
@@ -9,3 +9,30 @@ $(function() {
     });
 });
 
+fetch ('https://api-salta-de-turno.onrender.com/api/plantasmedicinales')
+.then(response => {
+    if(!response.ok){
+        throw new Error ('HTTP error: ' + response.status)
+    }
+    return response.json()
+})
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+    console.error(error)
+})
+
+fetch ('https://api-salta-de-turno.onrender.com/api/medicamentos')
+.then(response => {
+    if(!response.ok){
+        throw new Error ('HTTP error: ' + response.status)
+    }
+    return response.json()
+})
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+    console.error(error)
+})
