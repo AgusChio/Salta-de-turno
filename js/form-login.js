5d9584c4c655522bbe49d1311f97fc75c78e3869
AgusChio <agus.chio25790@gmail.com>
2023-12-17 14:12:06 -0300

    fix validators

--- js/form-login.js
+++ js/form-login.js
@@ -36,11 +36,24 @@ function togglePasswordVisibility(passwordFieldId) {
 }
 
 // Validaciones
+const validateEmail = (email) => {
+    if (email.trim() === "") return false;
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email.toLowerCase());
+};
+
+
 const validateEmpty = (value) => value != "";
 const validatePassword = (password) => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/.test(password);
 
-const checkField = (id, errorClass, validationFunc) => {
-    if (!validationFunc($(id).value)) {
+const checkField = (id, errorClass, validationFunc, emptyCheck = false) => {
+    const value = $(id).value;
+    let isValid = validationFunc(value);
+    if (emptyCheck && value === "") {
+        isValid = false;
+    }
+
+    if (!isValid) {
         removeClass(errorClass, "none");
         addClass(id, "invalid");
     } else {
@@ -51,31 +64,64 @@ const checkField = (id, errorClass, validationFunc) => {
 
 const checkPasswordsMatch = () => {
     const password = $("#password-signup").value;
-    const confirmPassword = $("#password-signup-confirm").value;
+    const confirmPassword = $("#confirm-password").value;
     if (password !== confirmPassword) {
         removeClass(".password-match-error", "none");
-        addClass("#password-signup-confirm", "invalid");
-        return false;
+        addClass("#confirm-password", "invalid");
     } else {
         addClass(".password-match-error", "none");
-        removeClass("#password-signup-confirm", "invalid");
-        return true;
+        removeClass("#confirm-password", "invalid");
     }
 };
 
 $("#form-signup").addEventListener("submit", (e) => {
     e.preventDefault();
-    checkField("#name", ".name-error", validateEmpty);
-    checkField("#user-signup", ".user-error", validateEmpty);
+    checkField("#name", ".name-error", validateEmpty, true);
+    checkField("#email-signup", ".email-error", validateEmail);
     checkField("#password-signup", ".password-error", validatePassword);
     checkPasswordsMatch();
 
-    if (Array.from(formLogin.querySelectorAll('.invalid')).length === 0) {
-        console.log('Formulario enviado con éxito');
+    if (Array.from($(".container-form-login .invalid")).length === 0) {
+        console.log('Formulario de registro enviado con éxito');
     }
 });
 
-$("#name").addEventListener("keyup", () => checkField("#name", ".name-error", validateEmpty));
-$("#user-signup").addEventListener("keyup", () => checkField("#user-signup", ".user-error", validateEmpty));
+$("#form-login").addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkField("#email-login", ".email-error", validateEmail);
+    checkField("#password", ".password-error", validatePassword);
+
+    if (Array.from($(".container-form-login .invalid")).length === 0) {
+        console.log('Formulario de inicio de sesión enviado con éxito');
+    }
+});
+
+$("#name").addEventListener("keyup", () => checkField("#name", ".name-error", validateEmpty, true));
+$("#email-login").addEventListener("keyup", () => checkField("#email-login", ".email-error", validateEmail, true));
 $("#password-signup").addEventListener("keyup", () => checkField("#password-signup", ".password-error", validatePassword));
+$("#email-signup").addEventListener("keyup", () => checkField("#email-signup", ".email-error", validateEmail, true));
 $("#password-signup-confirm").addEventListener("keyup", checkPasswordsMatch);
+
+const updateButtonState = () => {
+    const allLoginFieldsFilled = Array.from(document.querySelectorAll('#form-login input:not([type="checkbox"])')).every(input => input.value.trim() !== '');
+    const allLoginFieldsValid = Array.from(document.querySelectorAll('#form-login .invalid')).length === 0;
+    const isRememberChecked = $('#remember').checked;
+    $('#login-button').disabled = !(allLoginFieldsFilled && allLoginFieldsValid && isRememberChecked);
+    $('#login-button').classList.toggle('btn-login', !$('#login-button').disabled);
+    $('#login-button').classList.toggle('btn-login-disabled', $('#login-button').disabled);
+
+    const allSignupFieldsFilled = Array.from(document.querySelectorAll('#form-signup input:not([type="checkbox"])')).every(input => input.value.trim() !== '');
+    const allSignupFieldsValid = Array.from(document.querySelectorAll('#form-signup .invalid')).length === 0;
+    const isTermsAccepted = $('#acept').checked;
+    $('#signup-button').disabled = !(allSignupFieldsFilled && allSignupFieldsValid && isTermsAccepted);
+    $('#signup-button').classList.toggle('btn-login', !$('#signup-button').disabled);
+    $('#signup-button').classList.toggle('btn-login-disabled', $('#signup-button').disabled);
+};
+
+$('#remember').addEventListener('change', updateButtonState);
+$('#acept').addEventListener('change', updateButtonState);
+
+document.addEventListener('DOMContentLoaded', updateButtonState);
+document.querySelectorAll('#form-login input, #form-signup input').forEach(input => {
+    input.addEventListener('keyup', updateButtonState);
+});
