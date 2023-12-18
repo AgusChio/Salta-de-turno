3050717049839400908a9f9d8c661dccc50f00f9
AgusChio <agus.chio25790@gmail.com>
2023-12-18 09:02:27 -0300

    add disabled medicamento login

--- js/farmacias.js
+++ js/farmacias.js
@@ -20,3 +20,25 @@ function logout() {
 }
 
 window.onload = checkLoginStatus;
+
+// function deshabilitar Medicamento login
+function checkLoginStatus() {
+    var isLoggedIn = document.cookie.split(';').some((item) => item.trim().startsWith('username='));
+    var loginLink = document.getElementById('login-link');
+    var logoutLink = document.getElementById('logout-link');
+    var medicamentosLink = document.getElementById('medicamentos-link');
+
+    if (isLoggedIn) {
+        loginLink.style.display = 'none';
+        logoutLink.style.display = 'block';
+        medicamentosLink.href = "./pages/medicamentos.html";
+        medicamentosLink.classList.remove("disabled");
+    } else {
+        loginLink.style.display = 'block';
+        logoutLink.style.display = 'none';
+        medicamentosLink.removeAttribute("href");
+        medicamentosLink.classList.add("disabled");
+    }
+}
+
+window.onload = checkLoginStatus;
