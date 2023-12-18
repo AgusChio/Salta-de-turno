ff1faba805bf48e26ea18ef815efe72b9a1df93e
AgusChio <agus.chio25790@gmail.com>
2023-12-18 10:11:50 -0300

    add message in dropdown medicamento when is logged or not, and add in farmacias, medicamento and index html

--- js/farmacias.js
+++ js/farmacias.js
@@ -1,44 +1,35 @@
 function checkLoginStatus() {
-    console.log('Cookie username:', document.cookie.includes('username='));
-    var isLoggedIn = document.cookie.split(';').some((item) => item.trim().startsWith('username='));
-
-    var loginLink = document.getElementById('login-link');
-    var logoutLink = document.getElementById('logout-link');
-
-    if (isLoggedIn) {
-        loginLink.style.display = 'none';
-        logoutLink.style.display = 'block';
-    } else {
-        loginLink.style.display = 'block';
-        logoutLink.style.display = 'none';
-    }
-}
-
-function logout() {
-    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    checkLoginStatus();
-}
-
-window.onload = checkLoginStatus;
-
-// function deshabilitar Medicamento login
-function checkLoginStatus() {
     var isLoggedIn = document.cookie.split(';').some((item) => item.trim().startsWith('username='));
     var loginLink = document.getElementById('login-link');
     var logoutLink = document.getElementById('logout-link');
     var medicamentosLink = document.getElementById('medicamentos-link');
+    var medicamentosDropdown = document.getElementById('medicamentos-dropdown');
+
+    loginLink.style.display = isLoggedIn ? 'none' : 'block';
+    logoutLink.style.display = isLoggedIn ? 'block' : 'none';
 
     if (isLoggedIn) {
-        loginLink.style.display = 'none';
-        logoutLink.style.display = 'block';
-        medicamentosLink.href = "./pages/medicamentos.html";
-        medicamentosLink.classList.remove("disabled");
+        medicamentosLink.dataset.bsToggle = 'dropdown';
+        medicamentosDropdown.innerHTML = `
+            <li><a class="dropdown-item" href="./pages/medicamentos.html#free">Venta libre</a></li>
+            <li><a class="dropdown-item" href="./pages/medicamentos.html#recipes">Venta bajo receta</a></li>
+            <li><a class="dropdown-item" href="./pages/medicamentos.html#types-med">Tipos</a></li>
+            <li><a class="dropdown-item" href="./pages/medicamentos.html#injectables">Inyectables</a></li>
+            <li><a class="dropdown-item" href="./pages/medicamentos.html#plants">Plantas medicinales</a></li>
+        `;
     } else {
-        loginLink.style.display = 'block';
-        logoutLink.style.display = 'none';
-        medicamentosLink.removeAttribute("href");
-        medicamentosLink.classList.add("disabled");
+        medicamentosLink.dataset.bsToggle = 'dropdown';
+        medicamentosDropdown.innerHTML = `
+            <li><span class="dropdown-item dropdown-item-noLogued disabled" tabindex="-1" aria-disabled="true">Debes iniciar sesión y aceptar los términos y condiciones para acceder a esta sección.</span></li>
+        `;
     }
+}
+
+// Función para manejar el cierre de sesión
+function logout() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    checkLoginStatus();
+    window.location.href = '../index.html';
 }
 
 window.onload = checkLoginStatus;
