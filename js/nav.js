fb3772f113e0cb9314210e4b5f62ad310b0f324e
Laura Escalante <laura088.le@gmail.com>
2023-12-20 01:06:36 -0300

    arreglé el modo oscuro del login

--- js/nav.js
+++ js/nav.js
@@ -1,50 +1,65 @@
-const $ = (selector) => document.querySelector(selector)
+const $$ = (selector) => document.querySelector(selector)
 
 // localStorage
 document.addEventListener('DOMContentLoaded', function() {
     const modoAlmacenado = localStorage.getItem('modo')
     if (modoAlmacenado === 'oscuro') {
         document.body.classList.add('dark-mode')
-        $('#check').checked = true
-        $('.switch__label').innerText = 'Modo Oscuro'
-        $("#imgArriba").setAttribute("src", "./assets/images/olas-oscuro-arriba.png")
+        const check = document.querySelector(('#check'))
+        const switchLabel = document.querySelector(('.switch__label'))
+        const imgArriba = $$("#imgArriba")
+        if (imgArriba && check && switchLabel) {
+            switchLabel.innerText = 'Modo Oscuro'
+            check.checked = true
+            imgArriba.setAttribute("src", "./assets/images/olas-oscuro-arriba.png")
+        } 
     }
 })
 
-$('.navbar-toggler').addEventListener('click', () => {
+$$('.navbar-toggler').addEventListener('click', () => {
     $('.navbar-dropdown').classList.remove('none')
     $('.navbar-dropdown').classList.remove('hide')
 })
 
-$('.btn-close').addEventListener('click', () => {
+$$('.btn-close').addEventListener('click', () => {
     $('.navbar-dropdown').classList.add('none')
     $('.navbar-dropdown').classList.add('hide')
 })
 
 const settings = document.querySelector('.settings')
 
-$('.icon-settings').addEventListener('click', () => {
-    settings.classList.remove('none')
-    settings.classList.remove('hide')
-    $("#check").addEventListener("input", (e) => {
-        if(e.target.checked){ 
-            $('.switch__label').innerText = 'Modo Oscuro' 
-            document.body.classList.add('dark-mode') 
-            $("#imgArriba").setAttribute("src", "./assets/images/olas-oscuro-arriba.png")
-            localStorage.setItem('modo', 'oscuro')
-        }else{ 
-            $('.switch__label').innerText = 'Modo Claro'
-            document.body.classList.remove('dark-mode')
-            $("#imgArriba").setAttribute("src", "./assets/images/wave-white.png")
-            localStorage.setItem('modo', 'claro')
-        }
+if ($$('.icon-settings')) {
+    $$('.icon-settings').addEventListener('click', () => {
+        settings.classList.remove('none')
+        settings.classList.remove('hide')
+        $$("#check").addEventListener("input", (e) => {
+            if(e.target.checked){ 
+                $$('.switch__label').innerText = 'Modo Oscuro' 
+                document.body.classList.add('dark-mode') 
+                const imgArriba = $$("#imgArriba");
+                if (imgArriba) {
+                    imgArriba.setAttribute("src", "./assets/images/olas-oscuro-arriba.png");
+                }
+                localStorage.setItem('modo', 'oscuro')
+            }else{ 
+                $$('.switch__label').innerText = 'Modo Claro'
+                document.body.classList.remove('dark-mode')
+                const imgArriba = $$("#imgArriba");
+                if (imgArriba) {
+                    imgArriba.setAttribute("src", "./assets/images/wave-white.png");
+                }
+                localStorage.setItem('modo', 'claro')
+            }
+        })
     })
-})
+}
 
-$('.close-settings').addEventListener('click', () => {
-    settings.classList.add('none')
-    settings.classList.add('hide')
-})
+if ($$('.close-settings')) {
+    $$('.close-settings').addEventListener('click', () => {
+        settings.classList.add('none')
+        settings.classList.add('hide')
+    })
+}
 
 // Navbar Active 
 let linkElements = document.querySelectorAll('.nav-link')
@@ -57,8 +72,8 @@ for(let i=0; i<linkElements.length-1; i++){
 }
 
 // Scroll
-const navbar = $(".navbar")
-const navbarDrop = $(".navbar-dropdown")
+const navbar = $$(".navbar")
+const navbarDrop = $$(".navbar-dropdown")
 document.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         navbar.style.backgroundColor = 'var(--bg-nav-scroll)'
